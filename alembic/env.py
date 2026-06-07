0694c9bbd5b133da9933df1c71f373c2c49e0763
Krunal2802 <krunals27920@gmail.com>
2026-06-07 23:56:50 +0530

    Day7: Dockerize FastAPI application with PostgreSQL and Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,12 +5,24 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from dotenv import load_dotenv
+import os
+
 from app.models.base import Base
-from app.models.user_model import User
+from app.models.user import User
+
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    database_url
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
